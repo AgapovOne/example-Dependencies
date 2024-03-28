f7747eabc3d70befd6e531ca862b840cc450b430
AgapovOne <agapov.one@gmail.com>
2024-03-28 23:25:58 +0500

    Initial Commit

---
+++ Dependencies-example/Dependencies_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dependencies_exampleApp.swift
+//  Dependencies-example
+//
+//  Created by Alex Agapov on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Dependencies_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
